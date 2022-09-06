7d7df5e812e1cc49a3b6fc02342ca96df33b3be5
Diana Marcelin <marcelindiana@yahoo.com>
2022-09-06 18:59:39 -0400

    created components folder and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <p>Treat Yourself Today!</p>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <>
+      <footer>
+        <p>&copy; 2022, Diana Marcelin</p>
+      </footer>
+    </>
+  );
+}
+export default Footer;
