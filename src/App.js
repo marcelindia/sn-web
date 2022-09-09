531c8e8cd191a7bb738471c49b8ca9ba8f3443d5
Diana Marcelin <marcelindiana@yahoo.com>
2022-09-09 18:45:51 -0400

    frontend and backend are talking

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import "./App.css";
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
-      <p>Treat Yourself Today!</p>
+    <div className="App">
+      <header className="App-header">
+        <p>Treat Yourself Today!</p>
+        <button onClick={callApi}>Call API</button>
+      </header>
       <Footer />
     </div>
   );
+}
+
+function callApi() {
+  fetch("http://localhost:3001/details", { method: "GET" })
+    .then((data) => data.json())
+    .then((json) => alert(JSON.stringify(json)));
 }
 
 export default App;
